c5be771bff248d97875e6bfcb5c9264d24ee9398
axelldev <axellsolisrubio@gmail.com>
2026-02-15 16:52:15 -0600

    feat: implement oficial langchain-tavily search tool

--- main.py
+++ main.py
@@ -3,28 +3,13 @@ from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
-from tavily import TavilyClient
+from langchain_tavily import TavilySearch
 
 load_dotenv()
 
-tavily = TavilyClient()
-
-
-@tool
-def search(query: str) -> str:
-    """ "
-    Tool that searches the web for information.
-    Args:
-        query: The query to search the web fro.
-    Returns:
-        The information found on the web.
-    """
-    print(f"Searching the web for {query}")
-    return tavily.search(query=query)
-
 
 llm = ChatOpenAI(model="gpt-5-mini")
-tools = [search]
+tools = [TavilySearch()]
 agent = create_agent(model=llm, tools=tools)
 
 
